612a73ce92144044194458e68eb063cadf60a8c7
duliodelgado <dulio.delgado@gmail.com>
2024-05-04 14:19:47 -0500

    ok

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
   // Get the current year
-const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
-// Get the last modified date of the HTML document
-const lastModified = document.lastModified;
+  // Get the last modified date of the HTML document
+  const lastModified = document.lastModified;
 
-// Update the elements with IDs 'currentYear' and 'lastModified' with the retrieved values
-document.getElementById('currentYear').textContent = currentYear;
-document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
-
+  // Update the elements with IDs 'currentYear' and 'lastModified' with the retrieved values
+  document.getElementById('currentYear').textContent = currentYear;
+  document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+});
